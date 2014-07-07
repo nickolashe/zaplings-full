29c0f76780ab8bfa485660fd874039320533df00
Nickolay Shestopalov <nicko.shestopalov@gmail.com>
2014-07-07 07:19:54 +0000

    old models out

--- zaplings/models.py
+++ zaplings/models.py
@@ -4,16 +4,6 @@ from django.utils import timezone
 import datetime
 
 # Create your models here.
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-    def __unicode__(self):
-        return self.question
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
 class Idea(models.Model):
     creator = models.ForeignKey(User)
     title = models.CharField(max_length=200)
@@ -59,14 +49,6 @@ class UserOffer(models.Model):
 
       #def was_published_recently(self):
     #    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __unicode__(self):
-        return self.choice_text
 
 #class User(models.Model):
 #    email = models.EmailField(max_length=60)
